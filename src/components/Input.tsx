67f99d5cec78507b9118d02bde8fe7fe43421c8d
mimi <62527898+mimi-0506@users.noreply.github.com>
2025-01-03 22:10:44 +0900

    feat: data state에 따른 button css설정값 추가

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,11 +1,13 @@
 "use client";
 
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 import Image from "next/image";
 import "@/styles/components.css";
 import useSetDatas from "../hooks/useSetDatas";
+import { useTodoListStore } from "@/providers/store-provider";
 
 export default function Input() {
+  const { todos, dones } = useTodoListStore((state) => state);
   const { setDatas } = useSetDatas();
   const inputRef = useRef(null);
 
@@ -22,10 +24,25 @@ export default function Input() {
         className="input shape_1"
         placeholder="할 일을 입력해주세요"
       />
-      <button onClick={handleAdd} className="addButton shape_1">
-        <Image src="/svg/plus.svg" alt="Responsive" width="16" height="16" />
-        <p>추가하기</p>
-      </button>
+      {Array.isArray(todos) &&
+      todos.length === 0 &&
+      Array.isArray(dones) &&
+      dones.length === 0 ? (
+        <button onClick={handleAdd} className={`dataNone addButton shape_1`}>
+          <Image
+            src="/svg/plus_none.svg"
+            alt="Responsive"
+            width="16"
+            height="16"
+          />
+          <p>추가하기</p>
+        </button>
+      ) : (
+        <button onClick={handleAdd} className={`addButton shape_1`}>
+          <Image src="/svg/plus.svg" alt="Responsive" width="16" height="16" />
+          <p>추가하기</p>
+        </button>
+      )}
     </div>
   );
 }
