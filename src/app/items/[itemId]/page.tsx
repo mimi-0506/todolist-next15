3d21ed817bf92bb6f47dd13f3301167d15e18a5a
mimi <62527898+mimi-0506@users.noreply.github.com>
2025-01-04 17:27:51 +0900

    feat: detail page zustand 추가

--- src/app/items/[itemId]/page.tsx
+++ src/app/items/[itemId]/page.tsx
@@ -1,84 +1,51 @@
 "use client";
 
-import Image from "next/image";
 import "@/styles/detail.css";
-import useDataStateModify from "@/hooks/useDataStateModify";
 import useGetDetailData from "@/hooks/useGetDetailData";
-import { useEffect, useState } from "react";
-import { AreaLoading } from "@/components/Loading";
-import { modifyData } from "@/types/types";
+import { useEffect, useRef, useState } from "react";
+import { useTodoListStore } from "@/providers/store-provider";
 
 export default function Item({
   params,
 }: {
   params: Promise<{ itemId: string }>;
 }) {
-  const [data, setData] = useState<null | modifyData>(null);
+  const { detail, setDetail } = useTodoListStore((state) => state);
+
+  const [id, setId] = useState<null | string>(null);
+
+  const [loading, setLoading] = useState(false);
   const { getDetailData } = useGetDetailData();
-  const { modifyState } = useDataStateModify();
+
 
   useEffect(() => {
     getDatas();
   }, []);
 
   const getDatas = async () => {
-    const { itemId } = await params;
-    const nowData = await getDetailData(itemId);
-    setData(nowData);
+    if (id) {
+      const nowData = await getDetailData(id);
+      setDetail(nowData);
+    } else {
+      const { itemId } = await params;
+      const nowData = await getDetailData(itemId);
+      setDetail(nowData);
+      setId(itemId);
+    }
+  };
+
+  const startLoading = () => {
+    setLoading(true);
   };
 
   return (
     <div className="detailContainer dynamic">
-      {data ? (
+
+      {detail ? (
         <>
-          <div className={`${data.isCompleted ? "doneBackground" : ""} title`}>
-            <Image
-              src={data.isCompleted ? "/svg/done.svg" : "/svg/todo.svg"}
-              alt="Responsive"
-              width="32"
-              height="32"
-            />
-            <p>{data.name}</p>
-          </div>
           <div className="main">
-            <div className="imageContainer">
-              <Image
-                src={data.imageUrl ? data.imageUrl : "/svg/empty.svg"}
-                alt="Responsive"
-                width="64"
-                height="64"
-              />
-
-              <button>
-                <Image
-                  src={data.imageUrl ? "/svg/edit.svg" : "/svg/plus.svg"}
-                  alt="Responsive"
-                  width="24"
-                  height="24"
-                />
-              </button>
-            </div>
-
-            <div className="textContainer">
-              <h2>Memo</h2>
-              <p>{data?.memo}</p>
-            </div>
           </div>
           <div className="buttonContainer">
-            <button className="modifyButton shape_1">
-              <Image
-                src="/svg/check.svg"
-                alt="Responsive"
-                width="16"
-                height="16"
-              />
-              수정 완료
-            </button>
-
-            <button className="deleteButton shape_1">
-              <Image src="/svg/X.svg" alt="Responsive" width="16" height="16" />
-              삭제하기
-            </button>
           </div>
         </>
       ) : (
