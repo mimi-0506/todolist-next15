576bfad9ee3c0037ffe213c1d43dd0bbda796c5f
mimi <62527898+mimi-0506@users.noreply.github.com>
2025-01-04 17:21:34 +0900

    feat: detail zustand 추가

--- src/stores/store.ts
+++ src/stores/store.ts
@@ -1,13 +1,16 @@
+import { modifyData } from "@/types/types";
 import { createStore } from "zustand/vanilla";
 
 export type TodoListState = {
   todos: [] | null;
   dones: [] | null;
+  detail: modifyData | null;
 };
 
 export type TodoListActions = {
   setTodos: (newTodos: []) => void;
   setDones: (newDones: []) => void;
+  setDetail: (newDetail: modifyData) => void;
 };
 
 export type TodoListStore = TodoListState & TodoListActions;
@@ -15,6 +18,7 @@ export type TodoListStore = TodoListState & TodoListActions;
 export const defaultInitState: TodoListState = {
   todos: null,
   dones: null,
+  detail: null,
 };
 
 export const createTodoListStore = (
@@ -24,5 +28,6 @@ export const createTodoListStore = (
     ...initState,
     setTodos: (newTodos) => set((state) => ({ ...state, todos: newTodos })),
     setDones: (newDones) => set((state) => ({ ...state, dones: newDones })),
+    setDetail: (newDetail) => set((state) => ({ ...state, detail: newDetail })),
   }));
 };
